1fedd47d290e38d7c307aeb61e396a2eb22ef63c
seol1006a <seol1006@gmail.com>
2025-04-14 20:25:57 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() 
+{
+    int A;
+    int B;
+
+    scanf("%d %d", &A, &B);
+    if (A > B)
+    {
+        printf(">");
+
+    }
+    else if (A < B)
+    {
+        printf("<");
+
+    }
+    else if (A == B)
+    {
+        printf("==");
+    }
+     
+    return 0;
+}
