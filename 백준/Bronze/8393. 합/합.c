4f95466914f2829eb5533598c703eefbed01b23d
seol1006a <seol1006@gmail.com>
2025-04-14 20:49:42 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n;
+    int sum = 0;
+    scanf("%d", &n);
+
+
+    for (int i = 0; i <= n; i++)
+    {
+        sum += i;
+        
+    }
+    printf("%d", sum);
+    
+
+    return 0;
+}
