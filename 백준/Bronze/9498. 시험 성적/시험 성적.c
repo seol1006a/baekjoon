9b5c022cb98743d29dfa89936958232713050715
seol1006a <seol1006@gmail.com>
2025-04-14 20:29:20 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() 
+{
+    int num;
+    scanf("%d", &num);
+    if (num >= 90 && num <= 100)
+    {
+        printf("A");
+    }
+    else if (num >= 80 && num <= 89)
+    {
+        printf("B");
+    }
+    else if (num >= 70 && num <= 79)
+    {
+        printf("C");
+    }
+    else if (num >= 60 && num <= 69)
+    {
+        printf("D");
+    }
+    else
+    {
+        printf("F");
+    }
+
+    return 0;
+}
