1aa95d47986d1a444c52b85cee9db1d9b351840f
seol1006a <seol1006@gmail.com>
2025-04-14 21:53:51 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int j = 1; j <= n; j++)
+    {
+        for (int i = 0; i < j; i++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
